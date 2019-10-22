a806f583fc99b5846d3e4fc480ff500b05b2dba8
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-22 23:36:12 +0200

    add spanish language support

--- hello/hello.go
+++ hello/hello.go
@@ -5,13 +5,18 @@ import "fmt"
 const englishHelloPrefix = "Hello, "
 
 // Hello returns the first statement
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish" {
+		return "Hola, " + name + "!"
+	}
+
 	return englishHelloPrefix + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
