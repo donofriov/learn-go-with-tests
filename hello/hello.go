7bd0e486f715d2b3a9c4ef4bd34f7bf48c4f73be
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-22 23:48:02 +0200

    refactor extract greeting prefix functionality to a separate function

--- hello/hello.go
+++ hello/hello.go
@@ -16,8 +16,10 @@ func Hello(name string, language string) string {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name + "!"
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
@@ -25,9 +27,10 @@ func Hello(name string, language string) string {
 		prefix = germanHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name + "!"
+	return
 }
 
 func main() {
