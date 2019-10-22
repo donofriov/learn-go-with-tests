f7bd3216c5b72b90e18f699dcb63158af4a831a0
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-23 01:44:53 +0200

    refactor to use range and slices

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,10 +1,10 @@
 package sum
 
 // Sum takes in an array of numbers and returns the su,
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
