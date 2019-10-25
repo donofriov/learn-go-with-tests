af8928e8ebab3f35e94efc346662138ed7dac3b6
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-25 11:16:11 +0200

    refactor with Shape interface

--- structs-methods-interfaces/geometry.go
+++ structs-methods-interfaces/geometry.go
@@ -1,9 +1,31 @@
 package geometry
 
+import "math"
+
 // Rectangle struct has a height and a width
 type Rectangle struct {
 	Height float64
 	Width  float64
+}
+
+// Area method for Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+// Circle struct has a radius
+type Circle struct {
+	Radius float64
+}
+
+// Area method for Circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Shape interface returns Area
+type Shape interface {
+	Area() float64
 }
 
 // Perimeter takes a height and width and returns (h+w)*2
--- structs-methods-interfaces/geometry_test.go
+++ structs-methods-interfaces/geometry_test.go
@@ -15,12 +15,24 @@ func TestPerimeter(t *testing.T) {
 }
 
 // Now let's create a function called Area(width, height float64) which returns the area of a rectangle.
+// Our next requirement is to write an Area function for circles.
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", want, got)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
