b2c2ef865e73febcc450b803cf945802fd86b292
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-25 11:01:30 +0200

    refactor Perimeter and Area to use Rectangle struct

--- structs-methods-interfaces/perimeter.go
+++ structs-methods-interfaces/perimeter.go
@@ -1,11 +1,17 @@
 package geometry
 
+// Rectangle struct has a height and a width
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
 // Perimeter takes a height and width and returns (h+w)*2
-func Perimeter(height, width float64) float64 {
-	return (height + width) * 2
+func Perimeter(rectangle Rectangle) float64 {
+	return (rectangle.Height + rectangle.Width) * 2
 }
 
 // Area takes a height and width and returns h*w
-func Area(height, width float64) float64 {
-	return height * width
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Height * rectangle.Width
 }
--- structs-methods-interfaces/perimeter_test.go
+++ structs-methods-interfaces/perimeter_test.go
@@ -5,7 +5,8 @@ import "testing"
 // Suppose that we need some geometry code to calculate the perimeter of a rectangle given a height and width.
 // We can write a Perimeter(width float64, height float64) function, where float64 is for floating-point numbers like 123.45.
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -15,8 +16,9 @@ func TestPerimeter(t *testing.T) {
 
 // Now let's create a function called Area(width, height float64) which returns the area of a rectangle.
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
+	rectangle := Rectangle{12.0, 6.0}
+	got := Area(rectangle)
+	want := 72.0
 
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
