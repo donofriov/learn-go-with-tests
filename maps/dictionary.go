0b8fa30e5aca02263effc7107f60b24f24efb7b9
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-27 22:13:50 +0100

    refactor and dry err messaging

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,11 +5,14 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
+// ErrNotFound global error message
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 // Search method for Dictionary type
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
