e8bd918766e80e718eb67fe358ba05a7412094de
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-27 11:01:53 +0100

    Add Bitcoin type and refactor messages to use strings

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -2,18 +2,31 @@ package wallet
 
 import "fmt"
 
+// Bitcoin type from existing int
+type Bitcoin int
+
+// Stringer interface
+type Stringer interface {
+	String() string
+}
+
+// String method for type alias Bitcoin
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // Wallet struct
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // Deposit method for Wallet struct
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
 // Balance method for Wallet struct
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
