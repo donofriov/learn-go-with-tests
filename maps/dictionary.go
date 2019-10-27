408103360b8c63483ff3eaae15ef9b722c37600a
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-27 22:10:26 +0100

    add error check for when word doesn't exist

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,11 +1,18 @@
 package dictionary
 
+import "errors"
+
 // Dictionary type
 type Dictionary map[string]string
 
 // Search method for Dictionary type
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
 
 // Search function
