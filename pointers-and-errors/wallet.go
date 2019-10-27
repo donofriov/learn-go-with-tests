469f9ee0fa8a273bc452292032b8902ce3b2b4bd
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-27 11:11:09 +0100

    Add Withdraw method and DRY tests

--- pointers-and-errors/wallet.go
+++ pointers-and-errors/wallet.go
@@ -22,11 +22,15 @@ type Wallet struct {
 
 // Deposit method for Wallet struct
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
 // Balance method for Wallet struct
 func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+// Withdraw method for Wallet struct
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -1,22 +1,29 @@
 package wallet
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
 
-	got := wallet.Balance()
-
-	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(5))
+		assertBalance(t, wallet, Bitcoin(15))
+	})
 }
