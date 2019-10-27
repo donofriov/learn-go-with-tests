032a4c761623387da700d6a258bdd1ba19c913d0
Vincent D'Onofrio <vincent.donofrio@soul-cycle.com>
2019-10-27 12:08:02 +0100

    Add coverage for no error

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -26,6 +26,13 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertNoError := func(t *testing.T, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -33,9 +40,11 @@ func TestWallet(t *testing.T) {
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
-		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(5))
-		assertBalance(t, wallet, Bitcoin(15))
+		wallet := Wallet{Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
